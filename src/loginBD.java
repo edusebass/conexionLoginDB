a261d2600e447f9391a55df91388f088aecf0a6a
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-07-28 12:54:29 -0500

    LOGINCONEXION db

---
+++ src/loginBD.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class loginBD {
+    private JPanel rootLogin;
+    private JButton INGRESARButton;
+    private JTextField usuarioField1;
+    private JPasswordField passwordField1;
+    private JButton REGISTRARSEButton;
+
+    public String usuario;
+    public String password;
+
+    public static final String DB_URL = "jdbc:mysql://localhost/estudiantes";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root_bas3";
+
+    public loginBD() {
+        INGRESARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                usuario = usuarioField1.getText();
+                password = passwordField1.getText();
+
+                try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+                    String QUERY = "SELECT nombre FROM estudiantes WHERE nombre = ? AND password = ?";
+                    PreparedStatement stmt = conn.prepareStatement(QUERY);
+                    stmt.setString(1, usuario);
+                    stmt.setString(2, password);
+                    ResultSet rs = stmt.executeQuery();
+
+                    if (rs.next()) {
+                        JOptionPane.showMessageDialog(rootLogin, "Credenciales correctas");
+                    } else {
+                        JOptionPane.showMessageDialog(rootLogin, "Credenciales incorrectas.");
+                    }
+                } catch (SQLException x) {
+                    throw new RuntimeException(x);
+                }
+            }
+        });
+        REGISTRARSEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("loginBD");
+        frame.setContentPane(new loginBD().rootLogin);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/registroBD.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class registroBD {
+    private JButton GUARDARButton;
+    private JTextField userregistroField1;
+    private JPanel registroroot;
+    private JButton INICIARSESIONButton;
+    private JPasswordField contraseñaregistroField1;
+
+    public String usuario;
+    public String password;
+
+    public static final String DB_URL = "jdbc:mysql://localhost/estudiantes";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root_bas3";
+
+public registroBD() {
+    GUARDARButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            usuario = userregistroField1.getText();
+            password = contraseñaregistroField1.getText();
+            try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+
+
+            } catch (SQLException x) {
+                throw new RuntimeException(x);
+            }
+        }
+    });
+}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("registroBD");
+        frame.setContentPane(new registroBD().registroroot);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
